64e3540d1c12030e202e3ea64f6ada7b9f57656e
helloworlde <hellowoodes@gmail.com>
2022-10-21 18:11:04 +0800

    Add metrics for reporter

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/avast/retry-go/v4 v4.0.3
 	github.com/gorilla/websocket v1.4.2
 	github.com/grafana/loki-client-go v0.0.0-20210114103019-6057f29bbf5b
+	github.com/prometheus/client_golang v1.8.0
 	github.com/prometheus/common v0.15.0
+	github.com/sirupsen/logrus v1.6.0
 )
 
 require (
@@ -19,13 +21,13 @@ require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.2.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.8.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/procfs v0.2.0 // indirect
 	github.com/prometheus/prometheus v1.8.2-0.20201028100903-3245b3267b24 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 )
 
 func envOrDefault(env string, def string) string {
@@ -15,13 +17,34 @@ func envOrDefault(env string, def string) string {
 }
 
 func main() {
+	log.Info("开始启动 Clash Reporter")
+
 	clashHost := envOrDefault("CLASH_HOST", "localhost:9090")
 	clashToken := envOrDefault("CLASH_TOKEN", "")
 	lokiAddr := envOrDefault("LOKI_ADDR", "http://localhost:3100/loki/api/v1/push")
+	metricPort := envOrDefault("METRIC_PORT", "9001")
+
+	log.Info("CLASH_HOST: ", clashHost)
+	log.Info("CLASH_TOKEN: ", clashToken)
+	log.Info("LOKI_ADDR: ", lokiAddr)
+	log.Info("METRIC_PORT: ", metricPort)
 
 	go handleReport(lokiAddr, clashHost, clashToken)
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	<-sigCh
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(`<html>
+			<head><title>Clash Reporter</title></head>
+			<body>
+			<h1>Clash Reporter</h1>
+			<p><a href='/metrics'>Metrics</a></p>
+			</body>
+			</html>`))
+		if err != nil {
+			log.Println("运行 Reporter 错误", err)
+			os.Exit(1)
+		}
+	})
+	log.Printf("监控 Metrics： http://localhost:%s/metrics", metricPort)
+	log.Fatal(http.ListenAndServe(":"+metricPort, nil))
 }
